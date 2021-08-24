62d9c1ff8c891d7c20ba5d5da1ffdac6b4811834
SANJAY <sanjay@fisdom.com>
2021-08-24 16:03:27 +0530

    .

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Test extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>React Native Test component</Text>
+      </View>
+    )
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+    height: 200,
+    width: 200
   },
 });
